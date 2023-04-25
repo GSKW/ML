8e9159fe65e7d76e24832fdbf03b2d39821a1e6e
Mariia Trofimova <mary.vikhreva@gmail.com>
2023-04-25 08:18:22 +0400

    feat: prittify template for model architecture

--- practice/object_detection/yolo/model.py
+++ practice/object_detection/yolo/model.py
@@ -7,16 +7,10 @@ import torch.nn as nn
 
 """
 Implementation of Yolo (v1) architecture
-with slight modification with added BatchNorm.
-
-Information about architecture config:
-    Tuple is structured by (kernel_size, filters, stride, padding)
-    "M" is simply maxpooling with stride 2x2 and kernel 2x2
-    List is structured by tuples and lastly int with number of repeats
 """
 
 class CNNBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, **kwargs):
+    def __init__(self, in_channels: int, out_channels: int, **kwargs):
         super(CNNBlock, self).__init__()
         # YOUR CODE HERE
 
@@ -25,19 +19,27 @@ class CNNBlock(nn.Module):
 
 
 class Yolov1(nn.Module):
-    def __init__(self, in_channels=3, **kwargs):
+    def __init__(self,
+                 in_channels: int = 3,
+                 split_size: int = 7,
+                 num_boxes: int = 2,
+                 num_classes: int = 20):
         super(Yolov1, self).__init__()
         self.in_channels = in_channels
-        self.darknet = self._create_conv_layers()
-        self.fcs = self._create_fcs(**kwargs)
+        self.split_size = split_size
+        self.num_boxes = num_boxes
+        self.num_clases = num_classes
+
+        self.darknet = self._create_conv_layers(in_channels)
+        self.fcs = self._create_fcs(split_size, num_boxes, num_classes)
 
     def forward(self, x):
         x = self.darknet(x)
         return self.fcs(torch.flatten(x, start_dim=1))
 
-    def _create_conv_layers(self):
+    def _create_conv_layers(self, in_channels: int):
         # YOUR CODE HERE
 
-    def _create_fcs(self, split_size, num_boxes, num_classes):
+    def _create_fcs(self, split_size: int, num_boxes: int, num_classes: int):
         S, B, C = split_size, num_boxes, num_classes
         # YOUR CODE HERE
