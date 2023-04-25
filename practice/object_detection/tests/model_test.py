84705dc7c85f4080ec3e0f19d7e388ce976f1065
Mariia Trofimova <mary.vikhreva@gmail.com>
2023-04-25 11:26:18 +0400

    feat: add additional model tests

--- practice/object_detection/tests/model_test.py
+++ practice/object_detection/tests/model_test.py
@@ -21,6 +21,18 @@ class TestYolov1Model(unittest.TestCase):
         self.expected_darknet_shape = torch.Size((1, 1024, 7, 7))
         self.expected_output_shape = torch.Size((1, self.S * self.S * (self.C + self.B * 5)))
 
+        self.new_S = 10
+        self.expected_new_S_output_shape = \
+            torch.Size((1, self.new_S * self.new_S * (self.C + self.B * 5)))
+
+        self.new_B = 10
+        self.expected_new_B_output_shape = \
+            torch.Size((1, self.S * self.S * (self.C + self.new_B * 5)))
+
+        self.new_C = 21
+        self.expected_new_C_output_shape = \
+            torch.Size((1, self.S * self.S * (self.new_C + self.B * 5)))
+
     def test_darknet_layers(self):
         model = Yolov1(in_channels=3, split_size=self.S, num_boxes=self.B,
                        num_classes=self.C)
@@ -48,6 +60,27 @@ class TestYolov1Model(unittest.TestCase):
         output = model(self.yolo_gray_input)
         self.assertEqual(len(output.shape), len(self.expected_output_shape))
         self.assertEqual(output.shape, self.expected_output_shape)
+
+    def test_with_nondefault_split_size(self):
+        model = Yolov1(in_channels=3, split_size=self.new_S, num_boxes=self.B,
+                       num_classes=self.C)
+        output = model(self.yolo_rgb_input)
+        self.assertEqual(len(output.shape), len(self.expected_new_S_output_shape))
+        self.assertEqual(output.shape, self.expected_new_S_output_shape)
+
+    def test_with_nondefault_num_boxes(self):
+        model = Yolov1(in_channels=3, split_size=self.S, num_boxes=self.new_B,
+                       num_classes=self.C)
+        output = model(self.yolo_rgb_input)
+        self.assertEqual(len(output.shape), len(self.expected_new_B_output_shape))
+        self.assertEqual(output.shape, self.expected_new_B_output_shape)
+
+    def test_with_nondefault_num_classes(self):
+        model = Yolov1(in_channels=3, split_size=self.S, num_boxes=self.B,
+                       num_classes=self.new_C)
+        output = model(self.yolo_rgb_input)
+        self.assertEqual(len(output.shape), len(self.expected_new_C_output_shape))
+        self.assertEqual(output.shape, self.expected_new_C_output_shape)
 
 
 if __name__ == "__main__":
