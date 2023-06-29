7899c5cb2ab8b9d4c3f39188d455e8a92b80c668
Maria Trofimova <mary.vikhreva@gmail.com>
2023-06-29 07:44:55 +0300

    feat: add script to download contest data

---
+++ practice/contest/download_data.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget https://storage.yandexcloud.net/ds-ods/files/materials/8164c5a7/train_supervised_dataset.csv
+wget https://storage.yandexcloud.net/ds-ods/files/materials/df8a1e95/train_unsupervised_dataset.csv
+wget https://storage.yandexcloud.net/ds-ods/files/materials/60673a7c/test_dataset.csv
+wget https://storage.yandexcloud.net/ds-ods/files/materials/e04d6f74/sample_submission.csv
